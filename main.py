a36aa7ce830baa3b9148f7dfc7548775bb0dac36
SuperMeatBoyCoder <79085115+SuperMeatBoyCoder@users.noreply.github.com>
2023-04-28 18:19:27 +0300

    Update v.0.0.2
    Added: background, score
    Bugfixes

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ class GameSprite(sprite.Sprite): # Родительский класс для с
 class Player(GameSprite): # Игрок
     def left_update(self): # Движение по WASD
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_s] and self.rect.y < win_height-60:
+        if keys_pressed[K_s] and self.rect.y < win_height-110:
             self.rect.y += self.speed
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
     def right_update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_DOWN] and self.rect.y < win_height-60:
+        if keys_pressed[K_DOWN] and self.rect.y < win_height-110:
             self.rect.y += self.speed
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
@@ -41,7 +41,7 @@ class Ball(GameSprite):
         self.direction_x = (self.direction_x-0.5)*2
         
     def update(self):
-        global win
+        global win_l, win_r
         
         self.rect.y += self.speed*self.direction_y
         self.rect.x += self.speed*self.direction_x
@@ -52,16 +52,18 @@ class Ball(GameSprite):
             self.direction_x *= -1
 
         if self.rect.x < 0:
-            win = 2
-        elif self.rect.x > 700:
-            win = 1
+            win_r += 1
+            self.direction_x *= -1
+        elif self.rect.x > 670:
+            win_l += 1
+            self.direction_x *= -1
 
 
 # Приложение
-back = [0, 0, 0]
 win_width, win_height = 700, 500
 window = display.set_mode((win_width, win_height))
-window.fill(back)
+background = transform.scale(image.load('bg.jpg'), (700, 500))
+window.blit(background, (0,0))
 display.set_caption('Пинг-понг')
 
 # Надписи
@@ -69,16 +71,17 @@ font.init()
 font = font.Font(None, 36)
 
 # Объекты
-player_1 = Player('wall.jpg', speed=5, player_x=20, player_y=250, size_x=10, size_y=50)
-player_2 = Player('wall.jpg', speed=5, player_x=680, player_y=250, size_x=10, size_y=50)
-ball = Ball('ball.png', 5, 335, 235, 30, 30)
+player_1 = Player('platform.jpeg', speed=5, player_x=10, player_y=250, size_x=10, size_y=100)
+player_2 = Player('platform.jpeg', speed=5, player_x=680, player_y=250, size_x=10, size_y=100)
+ball = Ball('ball.png', speed=7, player_x=335, player_y=235, size_x=30, size_y=30)
 
 # Создание предметов
 clock = time.Clock()
 FPS = 30
 game = True
 finish = False
-win = 0
+win_l = 0
+win_r = 0
 while game:
     
     # Закрытие приложения по кнопке
@@ -97,17 +100,21 @@ while game:
     if finish != True:
         # Обновление
 
-        window.fill(back)
+        window.blit(background, (0,0))
         player_1.left_update()
         player_1.reset()
         player_2.right_update()
         player_2.reset()
         ball.update()
         ball.reset()
+        l_font = font.render('Счёт левого: ' + str(win_l), True, (255, 215, 0))
+        r_font = font.render('Счёт правого: ' + str(win_r), True, (255, 215, 0))
+        window.blit(r_font, (480, 0))
+        window.blit(l_font, (30, 0))
 
         # Выигрыш и проигрыш
-        if win != 0:
-            if win == 1:
+        if win_l >= 5 or win_r >= 5:
+            if win_l >= 5:
                 win_font = font.render('Победа левого!', True, (255, 215, 0))
             else:
                 win_font = font.render('Победа правого!', True, (255, 215, 0))
